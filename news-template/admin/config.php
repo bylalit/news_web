eefea577e7631da0ade32e6b9c32c93246a74876
Lalit0101 <lalitkp0101@gmail.com>
2024-09-24 19:48:54 +0530

    okk done

--- news-template/admin/config.php
+++ news-template/admin/config.php
@@ -1,4 +1,3 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","news-web") or die("Connection failed");
-    $hostname = "http://localhost/newss/news_web/news-template";
-?>
+$conn = mysqli_connect("localhost", "root", "", "news-web") or die("Connection failed");
+$hostname = "http://localhost/newss/news_web/news-template";
