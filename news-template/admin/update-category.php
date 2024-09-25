8af1acde02805815d112518923824c3cea125c99
Lalit0101 <lalitkp0101@gmail.com>
2024-09-25 11:52:35 +0530

    okk

--- news-template/admin/update-category.php
+++ news-template/admin/update-category.php
@@ -1,4 +1,13 @@
-<?php include "header.php"; ?>
+<?php include "header.php"; 
+
+    if(isset($_POST['sumbit'])){
+        $cat_id = $_POST['cat_id'];
+        $cat_name = $_POST['cat_name'];
+
+        
+    }
+
+?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -6,18 +15,29 @@
                   <h1 class="adin-heading"> Update Category</h1>
               </div>
               <div class="col-md-offset-3 col-md-6">
-                  <form action="" method ="POST">
+                <?php
+                    include "config.php";
+                    $id = $_GET['id'];
+                    $sql = "SELECT * FROM category WHERE category_id = '$id'";
+                    $result = mysqli_query($conn, $sql) or die("Connection Failed");
+                    while($row = mysqli_fetch_assoc($result)){
+                ?>
+                  <form action="<?php $_SERVER['PHP_SELF']; ?>" method ="POST">
                       <div class="form-group">
-                          <input type="hidden" name="cat_id"  class="form-control" value="1" placeholder="">
+                          <input type="hidden" name="cat_id"  class="form-control" value="<?php echo $row['category_id']; ?>" placeholder="">
                       </div>
                       <div class="form-group">
                           <label>Category Name</label>
-                          <input type="text" name="cat_name" class="form-control" value="Html"  placeholder="" required>
+                          <input type="text" name="cat_name" class="form-control" value="<?php echo $row['category_name']; ?>"  placeholder="" required>
                       </div>
                       <input type="submit" name="sumbit" class="btn btn-primary" value="Update" required />
                   </form>
+                  <?php
+                    }
+                  ?>
                 </div>
               </div>
+
             </div>
           </div>
 <?php include "footer.php"; ?>
