9b7fd1dde1504249cc7a9186e934eb3b3e40410b
Lalit0101 <lalitkp0101@gmail.com>
2024-10-06 21:36:48 +0530

    okk done

--- news-template/admin/add-post.php
+++ news-template/admin/add-post.php
@@ -7,7 +7,7 @@
              </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form -->
-                  <!-- <form  action="save-post.php" method="POST" enctype="multipart/form-data">
+                  <form  action="save-post.php" method="POST" enctype="multipart/form-data">
                       <div class="form-group">
                           <label for="post_title">Title</label>
                           <input type="text" name="post_title" class="form-control" autocomplete="off" required>
@@ -18,7 +18,7 @@
                       </div>
                       <div class="form-group">
                           <label for="exampleInputPassword1">Category</label>
-                          <select name="category" class="form-control"> -->
+                          <select name="category" class="form-control">
                               <option disabled> Select Category</option>
                               <?php
                                 include "config.php";
