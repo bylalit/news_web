cd519c2d1208b009f275cd9a6885af428e92d88e
Lalit0101 <lalitkp0101@gmail.com>
2024-10-27 21:51:56 +0530

    okk done the

--- news-template/admin/users.php
+++ news-template/admin/users.php
@@ -69,24 +69,24 @@ if($_SESSION["user_role"] == '0'){
                     $sql1 = "SELECT * FROM user";
                     $result1 = mysqli_query($conn, $sql1) or die("Query Failed!");
 
-                    // if(mysqli_num_rows($result1) > 0){
+                    if(mysqli_num_rows($result1) > 0){
 
-                    //     $total_recoard = mysqli_num_rows($result1);
+                        $total_recoard = mysqli_num_rows($result1);
                         
-                    //     $total_page = ceil($total_recoard / $limit);
+                        $total_page = ceil($total_recoard / $limit);
                         
-                    //     echo "<ul class='pagination admin-pagination'>";
-                    //     if($page > 1){
-                    //         echo '<li><a href="users.php?page='.($page - 1).'">Prev</a></li>';
-                    //     }
-                    //     for($i = 1; $i <= $total_page; $i++){
-                    //         if($i == $page){
-                    //             $active = "active";
-                    //         }else{
-                    //             $active = "";
-                    //         }
-                    //         echo '<li class="'.$active.'"><a href="users.php?page='.$i.'">'.$i.'</a></li>';
-                    //     }
+                        echo "<ul class='pagination admin-pagination'>";
+                        if($page > 1){
+                            echo '<li><a href="users.php?page='.($page - 1).'">Prev</a></li>';
+                        }
+                        for($i = 1; $i <= $total_page; $i++){
+                            if($i == $page){
+                                $active = "active";
+                            }else{
+                                $active = "";
+                            }
+                            echo '<li class="'.$active.'"><a href="users.php?page='.$i.'">'.$i.'</a></li>';
+                        }
                         if($total_page > $page){
                             echo '<li><a href="users.php?page='.($page + 1).'">Next</a></li>';
                         }
